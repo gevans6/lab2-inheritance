4dc76ffab8ac777d1ec4be8e71014bec02297d57
Grant Evans <grant.evans300@gmail.com>
2017-01-11 10:51:28 -0800

    finished part1

---
+++ lab2-part1.ts
@@ -0,0 +1,29 @@
+class CheckingAccount {
+    private balance:number;
+    private ownerName:string;
+
+    constructor(initialBalance:number, theOwnerName:string) {
+        this.balance = initialBalance;
+        this.ownerName = theOwnerName;
+    }
+
+    deposit(amount:number) {
+        this.balance += amount;
+    }
+
+    withdraw(amount:number) {
+        this.balance -= amount;
+    }
+
+    checkBalance() {
+        console.log(`${this.ownerName} has $${this.balance} in their account.`)
+    }
+}
+
+let ownerA:CheckingAccount = new CheckingAccount(1200, "Fred");
+let ownerB:CheckingAccount = new CheckingAccount(5000, "Bob");
+
+ownerA.deposit(500);
+ownerB.withdraw(200);
+ownerA.checkBalance();
+ownerB.checkBalance();
